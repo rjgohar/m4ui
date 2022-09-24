cd0d8a7c0a36976e36ae22942abc0961e525b13c
rjgohar <rehmatjan143@gmail.com>
2022-09-24 02:42:53 -0700

    ratting section complete

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import HeadSection from "../../components/headerSection";
+import SectionCount from "../../components/sectionCount";
 
 export default function Home() {
   return (
     <div>
       <HeadSection />
+
+      
     </div>
   );
 }
