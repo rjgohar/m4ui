94e78950241c20406d86b9adbbd8e2169e54b6a6
rjgohar <rehmatjan143@gmail.com>
2022-09-25 17:46:36 -0700

    add faq section

---
+++ src/components/FAQs/Data.js
@@ -0,0 +1,18 @@
+const Data = [
+  {
+    Question: " Where can i find the documentation?",
+    Answer:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  },{
+    Question: " Where can i find the documentation?",
+    Answer:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  },
+  {
+    Question: " Where can i find the documentation?",
+    Answer:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  },
+   
+];
+export default Data;
--- src/theme/palette.js
+++ src/theme/palette.js
@@ -12,5 +12,6 @@ export const palette = {
     main: "linear-gradient(86.73deg, #A4FB7B -10.91%, #242573 98.86%)",
  secondary:"linear-gradient(236.4deg, #A4FB7B 1.67%, #242573 148.2%)",
     primary: "linear-gradient(88.55deg, #A4FB7B -19.41%, #1B1772 115.44%)",
+  text:'linear-gradient(266.04deg, #242573 -4.02%, #A4FB7B 73.12%)'
   },
 };
